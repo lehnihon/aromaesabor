ebad150887fbbf67f283f9f9e7223ee8615ba4db
Leandro <lehnihon@hotmail.com>
2015-12-03 22:57:28 -0200

    sobre

--- page-sobre.php
+++ page-sobre.php
@@ -8,7 +8,7 @@ get_header(); ?>
 			<div class="row">
 				<div class="col-md-7 text-left">
 					<h4>Restaurante Aroma e Sabor</h4>
-					<p class="">Somos pioneiros no sistema de refeição a quilo na cidade de São Paulo.
+					<p>Somos pioneiros no sistema de refeição a quilo na cidade de São Paulo.
 						Atuamos há 26 anos, fornecendo comida caseira, num ambiente mais do que familiar.
 						<br>Tudo começou com Mama Odete e agora, já na segunda geração, o restaurante 
 						Aroma e Sabor procura manter o mesmo espírito e a mesma ideia: alimentar com 
@@ -35,10 +35,43 @@ get_header(); ?>
 						</div>
 			
 				</div>	
-			</div>		
+			</div>	
+			<div class="row">
+				<div class="col-md-12 text-left">
+					<h4>Locação para eventos</h4>
+					<p>Alugamos nosso espaço para eventos empresariais e particulares.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-left">
+					<h4>Ateliê Culinário</h4>
+					<p>Fornecemos bolos, salgados, tortas, lanches de metro, tudo para seus eventos e festas.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-left">
+					<h4>Serviços de catering</h4>
+					<p>Coffes break, churrascos, aniversários, feijoadas e coquetel.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-left">
+					<h4>Serviços de garçom</h4>
+					<p>Coperios, decoração, tudo para agilizar seus eventos.</p>
+				</div>
+			</div>	
 		</div>
 	</section>
 	
+	<section id="fotos-sobre">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					fotos
+				</div>
+			</div>
+		</div>
+	</section>
 
 	<?php get_template_part( 'template-parts/redes-bot'); ?>
 
