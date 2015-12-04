5047e93af52b2702d817cf08a797ccd7a77c80a0
Leandro <lehnihon@hotmail.com>
2015-12-04 10:31:19 -0200

    eventos criado

---
+++ page-eventos.php
@@ -0,0 +1,32 @@
+<?php
+get_header(); ?>
+
+<div id="content">
+	<section id="sobre">
+		<h1 class="title-full small">Eventos</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-left">
+					<h4 class="cor-base">Eventos</h4>
+					<p>Comemore seu evento no restaurante Aroma e Sabor. Alugamos nosso espaço para eventos empresariais e particulares. 
+						<br>O ambiente é muito agradável e tem vista panorâmica para o Mosteiro São Bento e viaduto Santa Ifigência. 
+						<br>Oferecemos um cardápio variado e saboroso, com bolos, salgados, tortas, doces e lanches de metro.
+						<br><br>Além do espaço você também conta com nossos serviços de buffet:
+					</p>
+					<ul>
+						<li>Serviços de Catering: coffes break, churrascos, aniversários, feijoadas e coqueteis</li>
+						<li>Serviços de garçom, copeiros, decoração, tudo para agilizar seus eventos</li>
+						<li>Encomendas sob medida</li>
+						<li>Cardápío saboroso e variado</li>
+					</ul>
+				</div>
+			</div>		
+		</div>
+	</section>
+	
+
+	<?php get_template_part( 'template-parts/redes-bot'); ?>
+
+</div>
+
+<?php get_footer(); ?>
