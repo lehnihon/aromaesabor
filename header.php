d4b7569c7ded4d6234625972bd18d8716c78b01c
Leandro <lehnihon@hotmail.com>
2016-04-10 20:19:07 -0300

    google

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="JRbUjmsCeL21Ij5oqZfB7r1llnyoCOTF_IFZcjZBDm0" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
